eb7ff519c4be301ecb986bac1f7311316aed3c98
italloVidal <itallovidalll@gmail.com>
2023-08-21 14:29:21 -0300

    limpando codigo

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -1,28 +1,17 @@
-import React, {useRef} from "react";
+import React from "react";
 import {cartItem} from "../../interfaces/interfaces";
-import {FlatList, StatusBar, } from "react-native";
+import { StatusBar, } from "react-native";
 import {ThemeProvider} from "styled-components/native";
 import {darkTheme} from "../../StyledComponents/theme.styled";
 import Header from "./components/Header/Header";
 import Form from "./components/Form/Form";
 import Product from "./components/Product/Product";
-import {Keyboard} from 'react-native'
 import * as Style from './Home.styled'
 import {KeyboardAwareFlatList} from "react-native-keyboard-aware-scroll-view";
 import FlatListHeader from "./components/FlatListHeader/FlatListHeader";
 
 function Home() {
     const [cartProducts, setCartProducts] = React.useState<Array<cartItem>>([])
-    const myList = React.useRef<FlatList>(null)
-    const [isShow, setIsShow] = React.useState(false)
-
-    Keyboard.addListener('keyboardDidHide',(e)=>{
-        setIsShow(false)
-    })
-
-    Keyboard.addListener('keyboardDidShow',()=>{
-        setIsShow(true)
-    })
 
     return (
         <ThemeProvider  theme={darkTheme}>
