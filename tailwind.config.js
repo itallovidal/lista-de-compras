01523e1bf364bc2c113022ebc947e8ddd27158b7
italloVidal <itallovidalll@gmail.com>
2024-08-28 01:57:40 -0300

    add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          700: '#2e83cb',
+          600: '#379DF1',
+        },
+        gray: {
+          700: '#121214',
+          600: '#202024',
+          500: '#29292E',
+          400: '#323238',
+        },
+        white: '#FFFFFF',
+        red: {
+          500: '#F75A68',
+        },
+      },
+    },
   },
   plugins: [],
 }
-
