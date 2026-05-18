9fce2cd20cebf60b1215e3e6ae5a8b864197c964
itallo <itallovidalll@gmail.com>
2026-05-18 17:10:54 -0300

    reset app

--- src/utils/save-actual-product-list.ts
+++
@@ -1,7 +0,0 @@
-import { IProduct } from '../@types/interfaces'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-
-export async function saveActualProductList(data: IProduct[]): Promise<void> {
-  const list = JSON.stringify(data)
-  await AsyncStorage.setItem('actual-product-list', list)
-}
